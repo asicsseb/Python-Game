03e862c4bb971a0da0836b52d71afe9f474ac7e7
asicsseb <64564524+asicsseb@users.noreply.github.com>
2022-02-10 18:41:06 -0600

    Add files via upload

--- working.py
+++ working.py
@@ -16,7 +16,7 @@ DRED = (255, 0, 0)
 
 
 def main():
-    global DISPLAYSURF, FPSCLOCK, mouseClicked, mX, mY, gameState, mainCharList
+    global DISPLAYSURF, FPSCLOCK, mouseClicked, mX, mY, gameState, mainCharList, candidate, stafferA, stafferB
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
     pygame.display.set_caption('Demagogues & Democracy')
@@ -32,14 +32,25 @@ def main():
 
     #scene switching variable
     gameState = 'title'
+
+    #Players Team
+    candidate = 'none'
+    stafferA = 'none'
+    stafferB = 'none'
+
     #Primary Game Loop
     while True:
         if gameState == 'title':
             gameState = titleScene()
         elif gameState == 'character select':
-            charScene()
+            changes = charScene()
+            gameState = changes[0]
+            candidate = changes[1]
         elif gameState == 'draft':
-            draftScene()
+            changes = draftScene()
+            gameState = changes[0]
+            stafferA = changes[1]
+            stafferB = changes[2]
         elif gameState == 'team':
             teamScene()
         elif gameState == 'area':
@@ -142,7 +153,6 @@ def titleScene():
 
 
 def charScene():
-    global mainCharacter
 
     BUTTONFONT = pygame.font.Font('freesansbold.ttf', 26)
     cbuttonList = []
@@ -151,47 +161,50 @@ def charScene():
     fontObj = pygame.font.Font('freesansbold.ttf', 48)
     textSurfaceObj = fontObj.render('Character Select', True, LRED)
     textRectObj = textSurfaceObj.get_rect()
-    textRectObj.center = (400, 150)
+    textRectObj.center = (400, 50)
 
     DISPLAYSURF.fill(BLACK)
     DISPLAYSURF.blit(textSurfaceObj, textRectObj)
 
-    mainCharacter = []
     itr = 0
-    x = WINWIDTH/3
-    y = 240
+    x = 400
+    y = 140
 
-    CHARABUTTON = [(x,y), (100, 100), 'charA']
-    CHARBBUTTON = [(x+120, y), (100, 100), 'charB']
-    CHARCBUTTON = [(x+240, y), (100, 100), 'charC']
+    CHARABUTTON = [(x-120,y), (100, 100), 'charA']
+    CHARBBUTTON = [(x, y), (100, 100), 'charB']
+    CHARCBUTTON = [(x+120, y), (100, 100), 'charC']
 
 
-    selection = 'none'
+    selection = candidate
     selectionFrame = [(400, 350), (100,100), ' ']
 
     createButton(BUTTONFONT, 'Char A', BLACK, LRED, CHARABUTTON[0], CHARABUTTON[1])
     cbuttonList.append(CHARABUTTON)
 
     createButton(BUTTONFONT, 'Char B', BLACK, LRED, CHARBBUTTON[0], CHARBBUTTON[1])
-    cbuttonList.append(CHARABUTTON)
+    cbuttonList.append(CHARBBUTTON)
 
     createButton(BUTTONFONT, 'Char C', BLACK, LRED, CHARCBUTTON[0], CHARCBUTTON[1])
-    cbuttonList.append(CHARABUTTON)
+    cbuttonList.append(CHARCBUTTON)
 
 
     #Next Button
     NEXTBUTTON = [(400, 500), (240, 80), 'draft']
     createButton(BUTTONFONT, 'Next', BLACK, LRED, NEXTBUTTON[0], NEXTBUTTON[1])
 
+    #Place holder variable to prevent selection frame from changing with clicks not relevant
+    holder = 'none'
+
+
     if mouseClicked == True:
         for button in cbuttonList:
-            selection = clickCheck(button, mX, mY)
-            if selection != 'oops':
-                break
-            else:
-                selection = 'none'
+            holder = clickCheck(button, mX, mY)
+            if holder != 'oops':
+                selection = holder
+                
+
         newState = clickCheck(NEXTBUTTON, mX, mY)
-        if newState != 'oops':
+        if newState != 'oops' and candidate != 'none':
             pass
         else: newState = 'character select'
 
@@ -205,11 +218,117 @@ def charScene():
     createButton(BUTTONFONT, selectionFrame[2], BLACK, LRED, selectionFrame[0], selectionFrame[1])
 
     main.mouseClicked = False
-    return newState
+    return newState, selection
 
 
 def draftScene():
-    a = 0
+
+    BUTTONFONT = pygame.font.Font('freesansbold.ttf', 26)
+    cbuttonList1 = []
+    cbuttonList2 = []
+    newState = 'draft'
+    #Staff Selection
+    fontObj = pygame.font.Font('freesansbold.ttf', 48)
+    textSurfaceObj = fontObj.render('Staffer Selection', True, LRED)
+    textRectObj = textSurfaceObj.get_rect()
+    textRectObj.center = (400, 50)
+
+    DISPLAYSURF.fill(BLACK)
+    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+
+    itr = 0
+    x = 400
+    y1 = 140
+    y2 = 260
+
+    STAFFABUTTON = [(x-120,y1), (100, 100), 'staffA']
+    STAFFBBUTTON = [(x, y1), (100, 100), 'staffB']
+    STAFFCBUTTON = [(x+120, y1), (100, 100), 'staffC']
+
+    STAFFDBUTTON = [(x-120,y2), (100, 100), 'staffD']
+    STAFFEBUTTON = [(x,y2), (100, 100), 'staffE']
+    STAFFFBUTTON = [(x+120,y2), (100, 100), 'staffF']
+
+
+    selection = candidate
+    staff1 = stafferA
+    staff2 = stafferB
+    selectionFrame = [(x-120, 380), (100,100), ' ']
+    staffFrame1 = [(x, 380), (100,100), ' ']
+    staffFrame2 = [(x+120, 380), (100, 100),' ']
+
+    createButton(BUTTONFONT, 'Staff A', BLACK, LRED, STAFFABUTTON[0], STAFFABUTTON[1])
+    cbuttonList1.append(STAFFABUTTON)
+
+    createButton(BUTTONFONT, 'Staff B', BLACK, LRED, STAFFBBUTTON[0], STAFFBBUTTON[1])
+    cbuttonList1.append(STAFFBBUTTON)
+
+    createButton(BUTTONFONT, 'Staff C', BLACK, LRED, STAFFCBUTTON[0], STAFFCBUTTON[1])
+    cbuttonList1.append(STAFFCBUTTON)
+
+    createButton(BUTTONFONT, 'Staff D', BLACK, LRED, STAFFDBUTTON[0], STAFFDBUTTON[1])
+    cbuttonList2.append(STAFFDBUTTON)
+
+    createButton(BUTTONFONT, 'Staff E', BLACK, LRED, STAFFEBUTTON[0], STAFFEBUTTON[1])
+    cbuttonList2.append(STAFFEBUTTON)
+
+    createButton(BUTTONFONT, 'Staff F', BLACK, LRED, STAFFFBUTTON[0], STAFFFBUTTON[1])
+    cbuttonList2.append(STAFFFBUTTON)
+
+
+    #Next Button
+    NEXTBUTTON = [(400, 500), (240, 80), 'team']
+    createButton(BUTTONFONT, 'Next', BLACK, LRED, NEXTBUTTON[0], NEXTBUTTON[1])
+
+    #Place holder variable to prevent selection frame from changing with clicks not relevant
+    holder = 'none'
+
+    if mouseClicked == True:
+        for button in cbuttonList1:
+            holder = clickCheck(button, mX, mY)
+            if holder != 'oops':
+                staff1 = holder
+                break
+        
+        for button in cbuttonList2:
+            print("This is running")
+            holder = clickCheck(button, mX, mY)
+            if holder != 'oops':
+                staff2 = holder
+                break
+
+        newState = clickCheck(NEXTBUTTON, mX, mY)
+        if newState != 'oops':
+            pass
+        else: newState = 'draft'
+
+    if selection == 'charA':
+        selectionFrame[2]= 'Char A'
+    elif selection == 'charB':
+        selectionFrame[2]= 'Char B'
+    elif selection == 'charC':
+        selectionFrame[2]= 'Char C'
+
+    if staff1 == 'staffA':
+        staffFrame1[2]= 'Staff A'
+    elif staff1 == 'staffB':
+        staffFrame1[2]= 'Staff B'
+    elif staff1 == 'staffC':
+        staffFrame1[2]= 'Staff C'
+    
+    if staff2 == 'staffD':
+        staffFrame2[2]= 'Staff D'
+    elif staff2 == 'staffE':
+        staffFrame2[2]= 'Staff E'
+    elif staff2 == 'staffF':
+        staffFrame2[2]= 'Staff F'
+
+    createButton(BUTTONFONT, selectionFrame[2], BLACK, LRED, selectionFrame[0], selectionFrame[1])
+    createButton(BUTTONFONT, staffFrame1[2], BLACK, LRED, staffFrame1[0], staffFrame1[1])
+    createButton(BUTTONFONT, staffFrame2[2], BLACK, LRED, staffFrame2[0], staffFrame2[1])
+
+    main.mouseClicked = False
+    return newState, staff1, staff2
 
 def teamScene():
     a = 0
